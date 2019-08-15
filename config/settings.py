38c9040516fb041d29370fe3c20ed2c0073ff0d3
JINUKK <askjw123@naver.com>
2019-08-15 23:59:17 +0900

    feat: settings.py // AWS S3 setting

--- config/settings.py
+++ config/settings.py
@@ -125,11 +125,39 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/' # 가상 URL
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+AWS_ACCESS_KEY_ID = 'AKIA3XAZBG73E2HWYFUD'
+AWS_SECRET_ACCESS_KEY = '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'
+
+AWS_REGION = 'ap-northeast-2'
+AWS_STORAGE_BUCKET_NAME = 'static.dstagram'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+# AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME
+AWS_S3_SECURE_URLS = False
+
+AWS_S3_FILE_OVERWRITE = False
+# True일 경우 같은 파일을 올렸을 때 덮어씌워진다.
+# False를 하면 같은 파일이 올라와도 덮어씌워지지 않게 파일 이름을 자동으로 바꿔줌
+
+# 브라우저가 해당 파일에 접속 했을 때 나타나는 파라미터 값
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+# 지금올리는 파일의 권한을 지정해줌
+AWS_DEFAULT_ACL = 'public-read'
+
+AWS_LOCATION = ''
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'config.s3media.MediaStorage'
+
+# MEDIA_URL = '/media/' # 가상 URL
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # 루트 폴더를 만들어줌으로써 앞으로 사진을 찾을 때 MEDIA_ROOT 경로 아래에서만 찾게 할 수 있다.
 # 루트 폴더가 자동으로 생김
 
